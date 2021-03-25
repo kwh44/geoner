d618ed15a0f85e4c02f039c16063cba4f5619787
kwh44 <maistruk44@gmail.com>
2021-03-25 22:30:06 +0200

    add graphics

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run -ti --name geoner --mount type=bind,source=/data/geoner,target=/home/geoner kwh44/geoner:latest bash
+docker run -ti --name geoner --mount type=bind,source=/data/geoner,target=/home/geoner \
+		--privileged \
+	    --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+	    --device=/dev/dri:/dev/dri \
+	    --env="DISPLAY=$DISPLAY" \
+	    --env QT_X11_NO_MITSHM=1 \
+		kwh44/geoner:latest bash
