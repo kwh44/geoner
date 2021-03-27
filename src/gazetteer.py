3c6195b32ab8a32bd33e7786baef99697996f0cc
kwh44 <maistruk44@gmail.com>
2021-03-27 20:54:44 +0200

    refactor

--- src/gazetteer.py
+++ src/gazetteer.py
@@ -1,9 +1,13 @@
 import requests
-import xml.etree.ElementTree as ET
 
-geonames_api = ["http://api.geonames.org/searchJSON?q=", "&maxRows=10&username=kwh44"]
-gisgraphy_api = ["https://services.gisgraphy.com/fulltext/fulltextsearch?q=paris"]
+geonames = {"api": "http://api.geonames.org/searchJSON?q=",
+            "format": "&maxRows=10&username=kwh44"}
 
-response = requests.get("https://services.gisgraphy.com/fulltext/fulltextsearch?q=london")
+gisgraphy = {"api": "https://services.gisgraphy.com/fulltext/fulltextsearch?q=",
+             "format": "&format=json"}
 
-print(response.content)
+url = ""
+
+response = requests.get(url)
+
+data = response.json()
