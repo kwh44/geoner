285f6a67aad7fd0962aebbe63c245824216ce57a
kwh44 <maistruk44@gmail.com>
2021-04-11 18:36:52 +0300

    extractor data added

--- src/extractor_nouns.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Tuple
-
-class ExtractorNouns:
-    def __init__(self, name: str):
-        self.name = name
-
-    def extract_places(self, text: str)-> List[Tuple[str, float]]:
-        pass
-
