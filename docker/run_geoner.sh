707195f54fb280787e9ece5a34fb2286f2dfc921
kwh44 <maistruk44@gmail.com>
2021-04-11 14:25:34 +0300

    format scripts

--- docker/run_geoner.sh
+++ docker/run_geoner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker run -ti --name geoner --mount type=bind,source=/data/geoner,target=/home/geoner \
-    --privileged \
-    -p 8888:8888 \
-    --volume=/tmp/.X11-unix:/tmp/.X11-unix \
-    --device=/dev/dri:/dev/dri \
-    --env="DISPLAY=$DISPLAY" \
-    --env QT_X11_NO_MITSHM=1 \
-	  kwh44/geoner:latest bash
+  --privileged \
+  -p 8888:8888 \
+  --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+  --device=/dev/dri:/dev/dri \
+  --env="DISPLAY=$DISPLAY" \
+  --env QT_X11_NO_MITSHM=1 \
+  kwh44/geoner:latest bash
